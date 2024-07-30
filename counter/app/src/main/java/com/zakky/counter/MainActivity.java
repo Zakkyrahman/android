9444b816529c606fdc2a30886f91a427ade2f510
Zakky <zakkyrahman1131@gmail.com>
2024-07-30 13:13:33 +0700

    vid 6
    projek counter

--- counter/app/src/main/java/com/zakky/counter/MainActivity.java
+++ counter/app/src/main/java/com/zakky/counter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.zakky.counter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int count = 0;
+     TextView tvhasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        load();
+
     }
+
+
+        public void load(){
+            tvhasil = findViewById(R.id.tvhasil);
+        }
+
+        public void btnup(View view){
+        count ++;
+            tvhasil.setText(count+"");
+        }
+
+        public void btndown(View view) {
+        count--;
+            tvhasil.setText(count+"");
+        }
 }
